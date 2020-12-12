492c54b8a46c9a8e21e9bbc65446e14e7333d99b
shimoda <kuri65536@hotmail.com>
2020-12-12 02:27:27 +0000

    simple cases works.

--- src/py_argumentparser.nim
+++ src/py_argumentparser.nim
@@ -1,7 +1,453 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+#[
+## license
+Copyright (c) 2020, shimoda as kuri65536 _dot_ hot mail _dot_ com
+                      ( email address: convert _dot_ to . and joint string )
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v.2.0. If a copy of the MPL was not distributed with this file,
+You can obtain one at https://mozilla.org/MPL/2.0/.
+]#
+import strformat
+import strutils
+import tables
+
+type  # {{{1
+  int_or_nil* = ref object of RootObj  # {{{1
+    value: int
+
+  float_or_nil* = ref object of RootObj  # {{{1
+    value: float
+
+  OptionDestType* = enum  # {{{1
+    str_single = 0
+    integer
+    float
+    boolean
+    str_seq
+
+  ActionResult* = enum  # {{{1
+    ok = 0
+
+  ActionExit* = enum  # {{{1
+    help = 1
+    version = 2
+
+  ArgumentType = enum  # {{{1
+    argument_is_value = 0
+    argument_is_option_without_value = 1
+    argument_is_option_with_value = 2
+
+  ActionFunc* = proc(key, val: string): ActionResult  # {{{1
+
+  OptionsAction* = ref object of RootObj  # {{{1
+    dest_type: OptionDestType
+    short_name: char
+    long_name, dest_name: string
+    action: ActionFunc
+    help_text: string
+
+  OptionsActionString* = ref object of OptionsAction  # {{{1
+    default: string
+    choices: seq[string]
+
+  OptionsActionBoolean* = ref object of OptionsAction  # {{{1
+    default: bool
+
+  OptionsActionInteger* = ref object of OptionsAction  # {{{1
+    default: int_or_nil
+
+  OptionsActionFloat* = ref object of OptionsAction  # {{{1
+    default: float_or_nil
+
+  ArgumentParser* = ref object of RootObj  # {{{1
+    help_header*: string
+    actions: seq[OptionsAction]
+
+  OptionBase* = ref object of RootObj  # {{{1
+    dest_type: OptionDestType
+
+  OptionString* = ref object of OptionBase  # {{{1
+    val*: string
+    choices: seq[string]
+    default: string
+
+  OptionInteger* = ref object of OptionBase  # {{{1
+    val: int
+
+  OptionFloat* = ref object of OptionBase  # {{{1
+    val: float
+
+  OptionBoolean* = ref object of OptionBase  # {{{1
+    val*: bool
+
+  OptionStrings* = ref object of OptionBase  # {{{1
+    vals: seq[string]
+
+  Options* = Table[string, OptionBase]  # {{{1
+
+
+var help_parser: ArgumentParser = nil
+var int_nil*: int_or_nil = nil
+var float_nil*: float_or_nil = nil
+
+
+proc initArgumentParser*(): ArgumentParser =  # {{{1
+    var ret = ArgumentParser(actions: @[])
+    return ret
+
+
+proc to_help(self: OptionsAction): string =  # {{{1
+    var ret = ""
+    if self.short_name != '\0':
+        ret = fmt"-{self.short_name}"
+    if len(self.long_name) > 0:
+        if len(ret) > 0: ret = ret & ", "
+        ret &= fmt"--{self.long_name}"
+    ret = fmt"    {ret:20}: " & self.help_text
+    return ret
+
+
+proc action_help*(key, val: string): ActionResult =  # {{{1
+    var head = help_parser.help_header
+    if len(head) < 1:
+        head = "exe"  # TODO(shimoda): get executable name
+    echo head
+    for act in help_parser.actions:
+        echo act.to_help()
+    system.quit(1)
+
+
+proc action_version*(key, val: string): ActionResult =  # {{{1
+    system.quit(1)
+
+
+proc set_opt_name(self: var OptionsAction, short, long, dest: string  # {{{1
+                  ): void =
+    self.short_name = '\0'
+    if len(short) < 1:
+        discard
+    elif len(short) == 1:
+        self.short_name = short[0]
+    elif short.startsWith("-"):
+        self.short_name = short[1]
+    else:
+        assert false
+
+    self.long_name = if long.startsWith("--"): long[2..^1]
+                     else:                     long
+    var name = if len(dest) > 0: dest
+               else:             self.long_name
+    if name.startsWith("--"):
+        name = name[2..^1]
+    if len(name) < 1 and self.short_name != '\0':
+        name = $self.short_name
+    self.dest_name = name
+    # echo fmt"set_opt_name: {self.dest_name} <= {short}, {long}"
+
+
+proc add_argument*(self: ArgumentParser,  # string {{{1
+                  opt_short, opt_long: string, default = "", dest = "",
+                  choices: seq[string] = @[], nargs = 0,
+                  action: ActionFunc = nil, help_text = ""): void =
+    var act = OptionsActionString(action: action, help_text: help_text,
+                                  default: default)
+    self.actions.add(act)
+    OptionsAction(act).set_opt_name(opt_short, opt_long, dest)
+
+    if len(choices) > 0:
+        act.choices = choices
+    if not isNil(action):
+        act.action = action
+
+
+proc add_argument(self: ArgumentParser,  # seq[string] {{{1
+                  opt_short, opt_long: string, default: seq[string],
+                  dest = "", nargs = 0,
+                  action: ActionFunc = nil): void =
+    # seq[string]
+    discard
+
+
+proc add_argument*(self: ArgumentParser,  # int {{{1
+                   opt_short, opt_long: string, default: int_or_nil, dest = "",
+                   action: ActionFunc = nil, help_text = ""): void =
+    var act = OptionsActionInteger(default: default, dest_type: OptionDestType.integer,
+                               action: action, help_text: help_text)
+    OptionsAction(act).set_opt_name(opt_short, opt_long, dest)
+    self.actions.add(act)
+
+    if not isNil(action):
+        act.action = action
+
+
+proc add_argument*(self: ArgumentParser,  # int {{{1
+                   opt_short, opt_long: string, default: int, dest = "",
+                   action: ActionFunc = nil, help_text = ""): void =
+    add_argument(self, opt_short, opt_long, int_or_nil(value: default), dest,
+                 action, help_text)
+
+
+proc add_argument*(self: ArgumentParser,  # float {{{1
+                   opt_short, opt_long: string, default: float_or_nil,
+                   dest = "", action: ActionFunc = nil, help_text = ""): void =
+    var act = OptionsActionFloat(
+            default: default, dest_type: OptionDestType.float,
+            action: action, help_text: help_text)
+    OptionsAction(act).set_opt_name(opt_short, opt_long, dest)
+    self.actions.add(act)
+
+    if not isNil(action):
+        act.action = action
+
+
+proc add_argument*(self: ArgumentParser,  # float
+                   opt_short, opt_long: string, default: float, dest = "",
+                   action: ActionFunc = nil, help_text = ""): void =
+    add_argument(self, opt_short, opt_long, float_or_nil(value: default), dest,
+                 action, help_text)
+
+
+proc add_argument*(self: ArgumentParser,  # bool {{{1
+                   opt_short, opt_long: string, default: bool,
+                   dest = "", nargs = 0,
+                   action: ActionFunc = nil, help_text = ""): void =
+    var act = OptionsActionBoolean(action: action, dest_type: OptionDestType.boolean,
+                                default: default, help_text: help_text)
+    OptionsAction(act).set_opt_name(opt_short, opt_long, dest)
+    self.actions.add(act)
+
+
+proc add_argument*(self: ArgumentParser,  # exit {{{1
+                   opt_short, opt_long: string, default: ActionExit,
+                   action: ActionFunc = nil, help_text = ""): void =
+    var act = OptionsActionBoolean(default: true, dest_type: OptionDestType.boolean,
+                                help_text: help_text)
+    OptionsAction(act).set_opt_name(opt_short, opt_long, "")
+    if isNil(action):
+        if default == ActionExit.help:
+            act.action = action_help
+        if default == ActionExit.version:
+            act.action = action_version
+    self.actions.add(act)
+
+
+method set_default(self: OptionsAction, opts: var Options): void {.base.} =  # {{{1
+    discard
+
+
+method set_default(self: OptionsActionString, opts: var Options): void =
+    var (name, val) = (self.dest_name, "")
+    if len(self.default) > 0:
+        val = self.default
+    elif len(self.choices) > 0:
+        val = self.choices[0]
+    else:
+        return
+
+    if len(opts) > 1 and opts.hasKey(name):
+        # info(fmt"set_default(override): {self.default} => {name}")
+        var opt = OptionString(opts[name])
+        opt.val = self.default
+    else:
+        # info(fmt"set_default: {self.default} => {name}")
+        opts.add(name, OptionString(val: val))
+
+
+method set_default(self: OptionsActionBoolean, opts: var Options): void =
+    opts[self.dest_name] = OptionBoolean(val: self.default)
+
+
+method set_default(self: OptionsActionInteger, opts: var Options): void =
+    if isNil(self.default):
+        return
+    opts[self.dest_name] = OptionInteger(val: self.default.value)
+
+
+method action_default(self: OptionsAction, opts: var Options,  # {{{1
+                      val: string): void {.base.} =
+    var name = self.dest_name
+    if self of OptionsActionInteger:
+        opts[name] = OptionInteger(val: parseInt(val),
+                               dest_type: OptionDestType.integer)
+    elif self of OptionsActionFloat:
+        opts[name] = OptionFloat(val: parseFloat(val),
+                                 dest_type: OptionDestType.float)
+    elif self of OptionsActionBoolean:
+        var act = OptionsActionBoolean(self)
+        opts[name] = OptionBoolean(val: not act.default,
+                                dest_type: OptionDestType.boolean)
+    #[ TODO(shimoda): not impemented, yet
+    elif self of OptionsActionStrings:
+        if not opts.hasKey(name):
+            opts[name] = OptionStrings(vals: @[val],
+                                       dest_type: OptionDestType.str_seq)
+        else:
+            try:
+                var tmp = OptionStrings()  # opts[name])
+                tmp.vals.add(val)
+            except:
+                discard
+    ]#
+
+
+method action_default(self: OptionsActionString, opts: var Options,
+                      val: string): void =
+    if len(self.choices) > 0:
+        if not self.choices.contains(val):
+            return
+    var name = self.dest_name
+    if opts.hasKey(name):
+        var opt = OptionString(opts[name])
+        opt.val = val
+    else:
+        var opt = OptionString(val: val, dest_type: OptionDestType.str_single)
+        opts.add(name, opt)
+
+
+method parse_one(self: OptionsAction,
+                 value: string): OptionBase {.base.} =
+    discard
+
+
+method parse_one(self: OptionsActionBoolean,
+                 value: string): OptionBase =
+    discard
+
+
+proc parse_known_args*(self: ArgumentParser, args: seq[string]  # {{{1
+                       ): tuple[opts: Options, args: seq[string]] =
+    var ret1 = initTable[string, OptionBase]()
+    var ret2: seq[string] = @[]
+
+    for act in self.actions:
+        act.set_default(ret1)
+
+    # debg("parse_args: long_no_vals: " & $longNoVals)
+
+    proc match_option(act: OptionsAction, s: char, l: string): int =  # {{{2
+        if s != '\0' and s != act.short_name:
+            return 0
+        if len(l) > 0 and l != act.long_name:
+            return 0
+        if act of OptionsActionBoolean:
+            return 1
+        return 2
+
+    proc parse_one_arg(arg: string  # {{{2
+                       ): tuple[typ: ArgumentType, act: OptionsAction] =
+        var s_name, l_name: string
+        if arg.startsWith("--"):
+            l_name = arg[2 ..^ 1]
+        elif not arg.startsWith("-"):
+            return (argument_is_value, nil)
+        else:
+            s_name = arg[1 ..^ 2]
+            for j in s_name:
+                for i in self.actions:
+                    var typ = match_option(i, j, "")
+                    if typ == 0:
+                        continue
+                    if typ == 1:
+                        return (argument_is_option_without_value, i)
+                    break  # skip options with value `ab` of like `-abc value`
+            s_name = arg[^1 .. ^1]
+
+        var short = if len(s_name) <= 0: '\0'
+                    else:               s_name[0]
+        for i in self.actions:
+            var n = match_option(i, short, l_name)
+            case n:
+            of 1:
+                return (argument_is_option_without_value, i)
+            of 2:
+                return (argument_is_option_with_value, i)
+            else:  # 0
+                continue
+        return (argument_is_value, nil)
+
+
+    #[
+    help_parser = self  # pass self to action_help()
+    debg(fmt"parse_one_opt: got {key}-{val}")
+    for opt in self.actions:
+        wste(fmt"parse_one_opt:check {key} vs {opt.dest_name}")
+        if key == opt.long_name:
+            discard
+        elif len(key) == 1 and key[0] == opt.short_name:
+            discard
+        else:
+            continue
+        wste(fmt"parse_one_opt: captured {opt.dest_name}")
+        if opt.dest_type == ActionDest.boolean:
+            OptionsActionBool(opt).run_action_bool(opts, key, val)
+            continue
+        elif isNil(opt.action):
+            discard
+        else:
+            var ret = opt.action(key, val)
+            # TODO(shioda): check ret, now ok only.
+            continue
+
+        # default actions = just store
+        opt.action_default(opts, val)
+        break
+    ]#
+
+    # loop {{{2
+    var arg_opt: OptionsAction = nil
+    for i in args:
+        if not isNil(arg_opt):
+            arg_opt.action_default(ret1, i)
+            arg_opt = nil
+            continue
+        var (typ, act) = parse_one_arg(i)
+        # echo "loop-check: ", typ, "-", i
+        case typ:
+        of argument_is_option_without_value:
+            act.action_default(ret1, "")
+        of argument_is_option_with_value:
+            arg_opt = act
+        else:
+            ret2.add(i)
+    return (ret1, ret2)
+
+
+proc parse_args*(self: ArgumentParser, args: seq[string]  # {{{1
+                 ): Options =
+    var (ret1, ret2) = self.parse_known_args(args)
+
+    if len(ret2) > 0:
+        raise newException(ValueError, "unknown arguments: " & $ret2)
+    return ret1
+
+
+proc get_string*(self: Options, name, default: string): string =  # {{{1
+    if self.hasKey(name):
+        var tmp = OptionString(self[name])
+        return tmp.val
+    return default
+
+
+proc get_boolean*(self: Options, name: string, default: bool): bool =  # {{{1
+    if self.hasKey(name):
+        var tmp = OptionBoolean(self[name])
+        return tmp.val
+    return default
+
+
+proc get_integer*(self: Options, name: string, default: int): int =  # {{{1
+    if self.hasKey(name):
+        var tmp = OptionInteger(self[name])
+        return tmp.val
+    return default
+
+
+proc get_float*(self: Options, name: string, default: float): float =  # {{{1
+    if self.hasKey(name):
+        var tmp = OptionFloat(self[name])
+        return tmp.val
+    return default
+
+
+# end of file {{{1
+# vi: ft=nim:et:ts=4:fdm=marker:nowrap
--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,128 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
+#[
+## license
+Copyright (c) 2020, shimoda as kuri65536 _dot_ hot mail _dot_ com
+                      ( email address: convert _dot_ to . and joint string )
 
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v.2.0. If a copy of the MPL was not distributed with this file,
+You can obtain one at https://mozilla.org/MPL/2.0/.
+
+## test matrix
+test case          | impl. | test num., etc
+-------------------|-------|-----------
+option common      |   |
+  short-opt        | o | 1-1-1, with and without '-'
+  no long-opt, dest-name  | o | 1-1-1, missing name
+  combined short   |   |
+  long-opt         | o | 1-1-2, with and without '--'
+  no short-opt, dest-name | o | 1-1-2
+  w/dest-name      | o | 1-1-2
+  nargs            |   |
+  action           |   |
+  action w/default |   | not implemented, yet.
+option boolean     |   |
+  simple           | o | 1-1-1, 1-1-2
+option string      | o | 1-2-1
+  from defaults    | o | 1-2-1
+  from choices     | o | 1-2-2
+  outside choides  | o | 1-2-2
+  as defaults      | o |
+  dupplicated      | x |
+option integer     | o | 1-3-1
+  program defaults | o | 1-3-1
+  default from get | o | 1-3-1
+option float       | o | 1-4-1
+  program defaults | o | 1-4-1
+  default from get | o | 1-4-1
+options merge      |   | see below
+  record->range    | o | 3-1
+]#
 import unittest
 
 import py_argumentparser
-test "can add":
-  check add(5, 5) == 10
+
+test "@T1-1-1 can parse boolean options(short)":
+    var p = initArgumentParser()
+    p.add_argument("b", "", default=false)
+    var (opts, vals) = p.parse_known_args(@["-a", "-b", "-c"])
+    check vals == @["-a", "-c"]
+    check opts.get_boolean("b", false) == true
+
+    p.add_argument("-a", "", default=false)
+    (opts, vals) = p.parse_known_args(@["-a", "-b", "-c"])
+    check vals == @["-c"]
+    check opts.get_boolean("a", false) == true
+
+test "@T1-1-2 can parse boolean options(long)":
+    var p = initArgumentParser()
+    p.add_argument("", "bonus", default=false)
+    var (opts, vals) = p.parse_known_args(@["--my", "--bonus", "--canceled"])
+    check vals == @["--my", "--canceled"]
+    check opts.get_boolean("bonus", false) == true
+
+    p.add_argument("", "--my", default=false)
+    (opts, vals) = p.parse_known_args(@["--my", "--bonus", "--canceled"])
+    check vals == @["--canceled"]
+    check opts.get_boolean("my", false) == true
+
+    p.add_argument("", "--canceled", dest="not", default=false)
+    (opts, vals) = p.parse_known_args(@["--my", "--bonus", "--canceled"])
+    check len(vals) < 1
+    check opts.get_boolean("not", false) == true
+
+test "@T1-2-1 can parse string options":
+    var p = initArgumentParser()
+    p.add_argument("", "test", default="abc")
+    var (opts, vals) = p.parse_known_args(@["--no", "--test", "--no", "--life"])
+    check vals == @["--no", "--life"]
+    check opts.get_string("test", "") == "--no"
+
+    (opts, vals) = p.parse_known_args(@["--life"])
+    check vals == @["--life"]
+    check opts.get_string("test", "") == "abc"
+
+test "@T1-2-2 can parse string from choices":
+    var p = initArgumentParser()
+    p.add_argument("", "no", default="",
+                   choices = @["air", "--test", "food"])
+    var (opts, vals) = p.parse_known_args(@["--no", "--test"])
+    check len(vals) < 1
+    check opts.get_string("no", "") == "--test"
+
+    # outside of choices
+    (opts, vals) = p.parse_known_args(@["--no", "from choice"])
+    check opts.get_string("no", "") == "air"
+
+test "@T1-3-1 can parse integer":
+    var p = initArgumentParser()
+    p.add_argument("", "this-num", default = -1)
+    var (opts, vals) = p.parse_known_args(@["show", "--this-num", "123"])
+    check vals == @["show"]
+    check opts.get_integer("this-num", -2) == 123
+
+    (opts, vals) = p.parse_known_args(@["no num were presented"])
+    check opts.get_integer("this-num", -3) == -1
+
+    p.add_argument("d", "no-default", default = int_nil)
+    (opts, vals) = p.parse_known_args(@[])
+    check opts.get_integer("no-default", -4) == -4
+
+test "@T1-4-1 can parse float":
+    var p = initArgumentParser()
+    p.add_argument("", "plus", default = 0.0)
+    var (opts, vals) = p.parse_known_args(@["--plus", "1.1", "1.2"])
+    check vals == @["1.2"]
+    check opts.get_float("plus", -1.1) == 1.1
+
+    (opts, vals) = p.parse_known_args(@[])
+    check opts.get_float("plus", -1.2) == 0.0
+
+    p.add_argument("m", "minus", default = int_nil)
+    (opts, vals) = p.parse_known_args(@["--minus", "2.2", "2.3"])
+    check opts.get_float("minus", -3.3) == 2.2
+
+    (opts, vals) = p.parse_known_args(@["--plus", "2.4", "2.5"])
+    check opts.get_float("minus", -3.4) == 3.4
+
+# end of file {{{1
+# vi: ft=nim:et:ts=4:fdm=marker:nowrap
