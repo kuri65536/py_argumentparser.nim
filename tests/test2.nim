8a21d9f4f107350dcad173f055f3a73788bab910
shimoda <kuri65536@hotmail.com>
2022-05-19 07:41:41 +0900

    add tests for failure cases in short options.

--- tests/test2.nim
+++ tests/test2.nim
@@ -10,11 +10,16 @@ You can obtain one at https://mozilla.org/MPL/2.0/.
 ## test matrix
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 test case                  | impl. | test num., etc
 ---------------------------|-------|-----------
 options '='                | o | 4-1-1
 options by '-abc'          | o | 4-2-1
 undefs by '-abc'           | o | 4-2-2
+options w/arg by '-a'      | o | 4-2-4
+wrong arg w/'-a'           | o | 4-2-3
+missing arg w/'-a'         | o | 4-2-5
+wrong by '-abc'            | o | 4-3-1, 4-3-2, 4-3-3
 ]##
 import unittest
 
@@ -49,14 +54,68 @@ test "T4-2-2 invalid single character options":
     check vals == @["-a", "-def=1", "-c"]
 
 
-test "T4-2-3 invalid single character options (mixed)":
+test "T4-2-3 invalid single character options (w/o arg, just ignored)":
+    var p = initArgumentParser()
+    p.add_argument('g', "g", default=false)
+    p.add_argument('h', "h", default=false)
+    p.add_argument('i', "i", default="")
+    var (opts, vals) = p.parse_known_args(@["-a", "-g=1", "-c"])
+    check opts.get_boolean("g") == true
+    check vals == @["-a", "-c"]
+
+
+test "T4-2-4 single character options (w/arg)":
+    var p = initArgumentParser()
+    p.add_argument('g', "g", default=false)
+    p.add_argument('h', "h", default=false)
+    p.add_argument('i', "i", default="")
+    var (opts, vals) = p.parse_known_args(@["-a", "-i", "-c"])
+    check opts.get_boolean("g") == false
+    check opts.get_boolean("h") == false
+    check opts.get_string("i") == "-c"
+    check vals == @["-a"]
+
+
+test "T4-2-5 single character options (w/arg, missing)":
+    var p = initArgumentParser()
+    p.add_argument('g', "g", default=false)
+    p.add_argument('h', "h", default=false)
+    p.add_argument('i', "i", default="")
+    var (opts, vals) = p.parse_known_args(@["-a", "-i"])
+    check vals == @["-a"]
+
+
+test "T4-3-1 invalid single character options (mixed)":
     var p = initArgumentParser()
     p.add_argument('g', "g", default=false)
     p.add_argument('h', "h", default=false)
     p.add_argument('i', "i", default="")
     var (opts, vals) = p.parse_known_args(@["-a", "-dhf=1", "-c"])
     check opts.get_boolean("h") == true
-    check vals == @["-a", "-dhf=1", "-c"]
+    check vals == @["-a", "-df=1", "-c"]
+
+
+test "T4-3-2 invalid single character options (mixed-2)":
+    var p = initArgumentParser()
+    p.add_argument('g', "g", default=false)
+    p.add_argument('h', "h", default=false)
+    p.add_argument('i', "i", default="")
+    var (opts, vals) = p.parse_known_args(@["-a", "-dhi=1", "-c"])
+    check opts.get_boolean("h") == true
+    check opts.get_string("i") == "1"
+    check vals == @["-a", "-d", "-c"]
+
+
+test "T4-3-3 invalid single character options (mixed-3)":
+    var p = initArgumentParser()
+    p.add_argument('g', "g", default=false)
+    p.add_argument('h', "h", default=false)
+    p.add_argument('i', "i", default="")
+    var (opts, vals) = p.parse_known_args(@["-a", "-dih=0", "-c"])
+    # =0 -> just ignored.
+    check opts.get_boolean("h") == true
+    check opts.get_string("i") == ""
+    check vals == @["-a", "-di", "-c"]
 
 
 # end of file {{{1
